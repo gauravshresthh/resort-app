6e8e6e0905dfb5fb3316228057fb86a87fcfb759
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-07-28 14:51:53 +0545

    hero , home and rooms updated

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Hero from '../components/Hero';
 const Rooms = () => {
-  return <div>Room</div>;
+  return (
+    <div>
+      Room
+      <Hero hero="roomsHero" />
+    </div>
+  );
 };
 
 export default Rooms;
