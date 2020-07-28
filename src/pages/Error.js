2d66695ad6d4032a9030c6c25c0887162d67aeb5
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-07-28 15:10:42 +0545

    router set on all of the components

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from './../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <>
+      <Hero>
+        <Banner tittle="404" subtitle="Page Not Found">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+    </>
+  );
 };
 
 export default Error;
