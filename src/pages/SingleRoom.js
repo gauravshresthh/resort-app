f86d8ce632776ac2ab1b1453770056851ef31525
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-14 22:24:59 +0545

    updated components

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 
 const SingleRoom = () => {
   return <div>Single room</div>;
