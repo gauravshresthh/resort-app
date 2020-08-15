6a8da7d3a333bae47437e08a13a1e67983ab763c
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-15 22:06:38 +0545

    filtering-complete

--- src/context.js
+++ src/context.js
@@ -53,14 +53,31 @@ class RoomProvider extends Component {
   }
 
   handleChange = (event) => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(name, type, value);
+    this.setState({ [name]: value }, this.filterRooms);
   };
 
   filterRooms = () => {
-    console.log('filter rooms');
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
   };
 
   render() {
